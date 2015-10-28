cd290158a58d3f71ca6cc89edb16710a0868cb8c
Joseph Ensminger <Jtensminger@hotmail.com>
2015-10-28 16:55:56 -0500

    Adds tests for index requests method

--- spec/requests/candidates_spec.rb
+++ spec/requests/candidates_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Candidates" do
   describe "GET /candidates" do
-    it "works! (now write some real specs)" do
-      get candidates_index_path
-      expect(response).to have_http_status(200)
+    describe "#index" do
+      it "shows candidates" do
+        Candidate.create!(name:"Obama", hometown:"Tennessee", district:"Nashville", party:"Democrat")
+        get Candidates_path
+        expect(response).to have_http_status(:success)
+        expect(json.count).to eq 1
+      end
     end
   end
 end
