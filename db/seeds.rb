6f2559e57718962d81ed926269760d835cde5a09
hLeePage <hleepage@yahoo.com>
2015-10-28 16:09:51 -0700

    Add entry in db.seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Candidate.create(name: 'Elmo', hometown: 'Denver', district:'23', party: 'Communist')
