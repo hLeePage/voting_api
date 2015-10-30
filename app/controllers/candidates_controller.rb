88cc83d6d373606699dfcd43726f671b70583c36
Joseph Ensminger <Jtensminger@hotmail.com>
2015-10-30 10:19:25 -0500

    added #show in candidates controller

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -3,4 +3,10 @@ class CandidatesController < ApplicationController
     @candidates = Candidate.all
     render json: @candidates
   end
+  
+  def show
+    @candidate = Candidate.find(params[:id])
+    render json: @candidate
+    #hi and thank you polish sausage! :D
+  end
 end
