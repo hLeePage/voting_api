4871c7392011f9312959a97717aa4f5f0855891d
Kurt Nelson <kurtisnelson@gmail.com>
2015-11-02 09:27:40 -0600

    Clean up tests

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -1,8 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Candidate do
-  it "checks if Candidate class exists" do
-    obama = Candidate.create!(name:"Obama", hometown:"Tennessee", district:"Nashville", party:"Democrat")
-    expect(obama.name).to eq("Obama")
-  end
 end
--- spec/requests/candidates_spec.rb
+++ spec/requests/candidates_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe "Candidates" do
   describe "GET /candidates" do
     describe "#index" do
       it "shows candidates" do
-        obama = Candidate.create!(name:"Obama", hometown:"Tennessee", district:"Nashville", party:"Democrat")
+        Candidate.create!(name:"Obama", hometown:"Tennessee", district:"Nashville", party:"Democrat")
         get candidates_path
         expect(response).to have_http_status(:success)
         expect(json.count).to eq 1
